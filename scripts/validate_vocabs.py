59ebd1eed0792cdadea15b4c208bf625b5ef2625
Nicholas Car <nick@kurrawong.net>
2024-10-16 18:06:12 +1000

    Pre4 ready

--- scripts/validate_vocabs.py
+++ scripts/validate_vocabs.py
@@ -1,36 +1,34 @@
 from pathlib import Path
 from pyshacl import validate
-import httpx
+from rdflib import Graph
 
-WARNINGS_INVALID = False  # Allows warnings to flag as invalid when true
+WARNINGS_INVALID = False # Allows warnings to flag as invalid when true
 SHOW_WARNINGS = True
+THIS_FILE = Path(__file__).resolve()
+ITEMS_DIR = THIS_FILE.parent.parent / "vocabularies"
 
+SHACL_GRAPH = Graph().parse("vocpub-4.10.ttl")
+
+CONTEXT_GRAPH = Graph().parse(THIS_FILE.parent.parent / "_background" / "agents.ttl")
 
 def main():
-    # get the validator
-    r = httpx.get("https://w3id.org/profile/vocpub/validator/4.7", follow_redirects=True)
-    assert r.status_code == 200
-    with open("vocpub-4.7.ttl", "w") as f:
-        f.write(r.text)
-
     # for all vocabs...
-    warning_vocabs = {}  # format {vocab_filename: warning_msg}
-    invalid_vocabs = {}  # format {vocab_filename: error_msg}
-    vocabs_dir = Path(__file__).parent.parent / "vocabularies"
-    for f in vocabs_dir.glob("**/*"):
+    warning_vocabs = {} # format {vocab_filename: warning_msg}
+    invalid_vocabs = {} # format {vocab_filename: error_msg}
+    for f in ITEMS_DIR.glob("*.ttl"):
         # ...validate each file
-        if f.name.endswith(".ttl"):
-            try:
-                v = validate(str(f), shacl_graph=r.text, shacl_graph_format="ttl")
-                if not v[0]:
-                    if "Severity: sh:Violation" in v[2]:
-                        invalid_vocabs[f.name] = v[2]
-                    elif "Severity: sh:Warning" in v[2]:
-                        warning_vocabs[f.name] = v[2]
+        try:
+            DATA_GRAPH = Graph().parse(f) + CONTEXT_GRAPH
+            v = validate(DATA_GRAPH, shacl_graph=SHACL_GRAPH, allow_warnings=True)
+            if not v[0]:
+                if "Severity: sh:Violation" in v[2]:
+                    invalid_vocabs[f.name] = v[2]
+                elif "Severity: sh:Warning" in v[2]:
+                    warning_vocabs[f.name] = v[2]
 
-            # syntax errors crash the validate() function
-            except Exception as e:
-                invalid_vocabs[f.name] = e
+        # syntax errors crash the validate() function
+        except Exception as e:
+            invalid_vocabs[f.name] = e
 
     # check to see if we have any invalid vocabs
     if len(warning_vocabs.keys()) > 0 and SHOW_WARNINGS:
@@ -48,15 +46,9 @@ def main():
             print(error)
             print("-----")
 
-    ret = True
     if WARNINGS_INVALID:
-        if len(warning_vocabs.keys()) > 0:
-            print("Warning vocabs: {}".format(", ".join([str(x) for x in warning_vocabs.keys()])))
-            ret = False
-    if len(invalid_vocabs.keys()) > 0:
-        print("Invalid vocabs: {}".format(", ".join([str(x) for x in invalid_vocabs.keys()])))
-        ret = False
-    assert ret
+        assert len(warning_vocabs.keys()) == 0, "Warning vocabs: {}".format(", ".join([str(x) for x in warning_vocabs.keys()]))
+    assert len(invalid_vocabs.keys()) == 0, "Invalid vocabs: {}".format(", ".join([str(x) for x in invalid_vocabs.keys()]))
 
 
 if __name__ == "__main__":
